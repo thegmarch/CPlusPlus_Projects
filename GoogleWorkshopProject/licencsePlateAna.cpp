261c84e37e03bc73f4f53670d60573f3fe153742
thegmarch <thegmarch@gmail.com>
2018-04-09 12:59:40 -0700

    Update licencsePlateAna.cpp

--- GoogleWorkshopProject/licencsePlateAna.cpp
+++ GoogleWorkshopProject/licencsePlateAna.cpp
@@ -14,11 +14,17 @@
 #include<unordered_map>
 using namespace std;
 
+/*
+ * Sort word by length: Sorting by length will also maintain alphabetical order
+ */
 bool compLen(string &a, string &b)
 {
 	return (a.size() < b.size());
 }
 
+/*
+ * Find frequency of letter in string 
+ */
 int findFreq(string &a, char &b)
 {
 	int count = 0;
@@ -63,13 +69,15 @@ int main(){
 	while(invalidLicensePlateFound)
 	{
 
-		cout << "Please Enter License Plate 5-7 characters: ";
+		cout << "Please Enter License Plate 5-7 characters in length with a few letters: ";
 		cin >> inputLicensePlate;
 		count = inputLicensePlate.length();
 		cout << count << endl;
 		//regex_match(inputLicensePlate,m,letter);
 		//mcount = m.size();
 		//cout << m.length() << endl;
+		
+		// Accept strings that are 6 to 8 characters in length
 		if((count >= 6 && count <= 8))
 		{
 		//	cout << "valid string" << endl;
@@ -94,8 +102,11 @@ int main(){
 	}
 	*/
 	bool checkCharInWord = false;
+	
+	//For each word in dictionary check if there is a letter frequency match
 	for(auto &j: dictionary)
 	{
+		//Verify letter frequency in license plate matches that of the word in dictionary
 		for(auto &i: plateChars)
 		{
 			if((j.find(i) != -1) && (findFreq(j,i) >= letFreq[i]))
@@ -108,6 +119,7 @@ int main(){
 				break;
 			}
 		}
+		//Letter frequency match, at to potential words to return 
 		if(checkCharInWord)
 		{
 			potWords.push_back(j);
